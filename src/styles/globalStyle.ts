560c8d872124f8826cbe34f7eaf8f2965bead5d9
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-17 21:35:34 +0300

    feat: add base styles for btn, list and visually hidden

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -7,7 +7,6 @@ const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
   body {
     box-sizing: border-box;
     overflow-x: hidden;
-    overflow-y: scroll;
     padding: 0;
     margin: 0;
     color: white;
@@ -19,6 +18,7 @@ const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
   a {
     color: inherit;
     text-decoration: none;
+    transition: color 0.3ms ease-in-out;
 
     &:hover {
       color: ${(props) => props.theme.colors.brand};
@@ -35,6 +35,32 @@ const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
   ::selection {
     color: white;
     background-color: ${(props) => props.theme.colors.brand};
+  }
+
+  .btn-reset {
+    padding: 0;
+    background: transparent;
+    cursor: pointer;
+    border: none;
+  }
+
+  .list-reset {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .visually-hidden:not(:focus):not(:active) {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    white-space: nowrap;
+    clip: rect(0 0 0 0);
+    clip-path: inset(100%);
+    border: 0;
+    overflow: hidden;
   }
 `
 
