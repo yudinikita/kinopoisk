1238faa4c525f322e439701bab56a94f3efc9f40
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-17 21:36:05 +0300

    feat: add space sizes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -19,6 +19,17 @@ const theme = {
     },
     family: "'Inter', sans-serif",
   },
+  border: {
+    base: '15px',
+  },
+  space: {
+    xs: '375px',
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+    xxl: '1400px',
+  },
 }
 
 export default theme
