8f22b13e08114062f8e2c2365e1211b8f215ee72
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-17 21:36:53 +0300

    feat: add global font styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'styles/globalStyle'
 import theme from 'styles/theme'
 import Layout from 'components/Layout/Layout'
+import FontStyles from 'styles/fontStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <FontStyles />
         <Layout>
           <Component {...pageProps} />
         </Layout>
