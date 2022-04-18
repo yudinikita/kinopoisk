3f262b648f24f227b80ae5dee46c2ae5ab2d4758
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-18 23:23:40 +0300

    feat: add template film carousel section on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import PromoSlider from 'components/PromoSlider/PromoSlider'
+import FilmCarouselSection from 'components/FilmCarousel/FilmCarouselSection'
 
 const Home: NextPage = () => {
   return (
     <div>
       <PromoSlider />
+      <FilmCarouselSection title="Билеты в кино" slides={[]} />
     </div>
   )
 }
