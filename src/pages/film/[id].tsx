ec0b17cb5e4590dd96207dde50939202aef5a269
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-21 19:52:26 +0300

    feat(film-detail): add film detail template on page

--- src/pages/film/[id].tsx
+++ src/pages/film/[id].tsx
@@ -1,12 +1,37 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { listFilms } from 'mocks/lists/filmsList'
+import FilmLayout from 'components/Layout/FilmDetail/FilmLayout'
+import FilmDetailSidebar from 'components/FilmDetail/Sidebar/FilmDetailSidebar'
+import FilmDetailMain from 'components/FilmDetail/Main/FilmDetailMain'
+import FilmDetailAdditional from 'components/FilmDetail/Additional/FilmDetailAdditional'
+import FilmDetailPlayer from 'components/FilmDetail/Player/FilmDetailPlayer'
+import FilmCarouselSection from 'components/FilmCarousel/FilmCarouselSection'
+import { loadFilms } from 'api/films'
+import type { FilmCardProps } from 'components/FilmCard/FilmCard'
 
 interface FilmProps {
   paramsId?: string
+  similarFilms?: FilmCardProps[]
 }
 
-const Film: NextPage = ({ paramsId }: FilmProps) => {
-  return <div>Film Page #{paramsId}</div>
+const Film: NextPage = ({ paramsId, similarFilms }: FilmProps) => {
+  return (
+    <FilmLayout
+      meta={{
+        title: 'Название фильма #' + paramsId,
+      }}
+      sidebar={<FilmDetailSidebar />}
+      main={<FilmDetailMain />}
+      additional={<FilmDetailAdditional />}
+      player={<FilmDetailPlayer />}
+      similar={
+        <FilmCarouselSection
+          title="Похожие фильмы"
+          slides={similarFilms ?? []}
+        />
+      }
+    />
+  )
 }
 
 export default Film
@@ -21,9 +46,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const similarFilms = await loadFilms({ type: 'recommendation', count: '10' })
+
   return {
     props: {
       paramsId: params?.id ?? '',
+      similarFilms,
     },
   }
 }
