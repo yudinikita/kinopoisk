3e88ac607f779c2b7a8fc5fe0815203442459498
Nikita Yudin <nikitayudin782@gmail.com>
2022-04-23 18:26:23 +0300

    feat(serials): add styles for responsive

--- src/pages/lists/serials.tsx
+++ src/pages/lists/serials.tsx
@@ -43,6 +43,19 @@ const Container = styled.div`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   row-gap: 50px;
   padding-bottom: 50px;
+
+  @media (max-width: ${(props) => props.theme.space.xl}) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (max-width: ${(props) => props.theme.space.md}) {
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+  }
+
+  @media (max-width: ${(props) => props.theme.space.sm}) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const Title = styled.h1`
@@ -50,4 +63,8 @@ const Title = styled.h1`
   margin-bottom: 20px;
   font-weight: 500;
   font-size: 44px;
+
+  @media (max-width: ${(props) => props.theme.space.md}) {
+    font-size: 28px;
+  }
 `
